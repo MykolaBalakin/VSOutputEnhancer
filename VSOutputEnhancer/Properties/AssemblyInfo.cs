d63c2bb4eba9d12f035040bc228e7949ce2b7e77
Nikolay Balakin <n@balakin.me>
2015-11-02 13:40:23 +0200

    Fix assembly metadata fields

--- VSOutputEnhancer/Properties/AssemblyInfo.cs
+++ VSOutputEnhancer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSColorfullOutput")]
+[assembly: AssemblyTitle("VSOutputEnhancer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSColorfullOutput")]
+[assembly: AssemblyProduct("VSOutputEnhancer")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
