864d47132123f886d5bbdcf2fd17959fe18d5f6b
Nikolay Balakin <n@balakin.me>
2015-11-05 20:39:08 +0200

    Move Utils class to separate file

--- VSOutputEnhancer/StyleManager.cs
+++ VSOutputEnhancer/StyleManager.cs
@@ -95,13 +95,4 @@ namespace Balakin.VSOutputEnhancer {
             return null;
         }
     }
-
-    internal static class Utils {
-        public static Double GetBrightness(this Color c) {
-            return Math.Sqrt(
-                c.R * c.R * .241 +
-                c.G * c.G * .691 +
-                c.B * c.B * .068) / 255;
-        }
-    }
 }
