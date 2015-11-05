505b92deadeff888065360bcab7fb306a77412d3
Nikolay Balakin <n@balakin.me>
2015-11-05 19:57:27 +0200

    Add info output in deploy script

--- DeployReleaseToGitHub.ps1
+++ DeployReleaseToGitHub.ps1
@@ -15,31 +15,43 @@ $path = $Env:BUILD_ARTIFACTSTAGINGDIRECTORY
 
 # Get version
 $manifestPath = [System.IO.Path]::Combine($path, "extension.vsixmanifest")
+Write-Host "Getting version from manifest ""$($manifestPath)""..."
 $manifest = [xml]$(Get-Content $manifestPath)
 $version = $manifest.PackageManifest.Metadata.Identity.Version
+Write-Host "    $($version)"
 
 
 # Create release
 $request = @{}
-$request.tag_name = "v" + $version
+$request.tag_name = $version
 $request.target_commitish = "master"
 $request.name = "VSOutputEnhancer " + $request.tag_name
+$request.body = ""
 $request.draft = $true
 $request.prerelease = $false
+Write-Host "Creating new release with parameters:"
+$request | Format-Table
 
 $content = ConvertTo-Json $request
 
 $webResponse = Invoke-WebRequest -Uri $uri -Method POST -Headers @{ "Authorization" = "token " + $Token } -Body $content -UseBasicParsing
+Write-Host "API response code: $($webResponse.StatusCode)"
 
 $release = ConvertFrom-Json $webResponse.Content
+Write-Host "Release created:"
+$release | Format-Table
 
 
 # Upload file
 $vsixName = "Balakin.VSOutputEnhancer.vsix"
 $releaseVsixName = "VSOutputEnhancer.vsix"
 $vsixPath = [System.IO.Path]::Combine($path, $vsixName)
+Write-Host "Uploading vsix package to GitHub..."
+Write-Host "Package path ""$($vsixPath)"""
+Write-Host "    size: $($(Get-Item $vsixPath).Length)"
 $uploadAssetUri = $release.upload_url -Replace "\{\?name,label\}", ("?name=" + $releaseVsixName)
 $webResponse = Invoke-WebRequest -Uri $uploadAssetUri -Method POST -ContentType "application/zip" -Headers @{ "Authorization" = "token " + $Token } -Body $(Get-Content $vsixPath) -UseBasicParsing
+Write-Host "API response code: $($webResponse.StatusCode)"
 
 
 # Commit release
