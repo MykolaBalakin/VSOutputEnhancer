3d92b37dae54c544e60b98143002929eea43ec1a
Nikolay Balakin <n@balakin.me>
2015-11-06 22:43:03 +0200

    Implement parsing of "Rebuild All" result

--- UnitTests/BuildResultParser.cs
+++ UnitTests/BuildResultParser.cs
@@ -8,18 +8,32 @@ namespace Balakin.VSOutputEnhancer.UnitTests {
     [TestClass]
     public class BuildResultParser {
         const String BuildResultMessage = "========== Build: 3 succeeded, 1 failed, 16 up-to-date, 5 skipped ==========\r\n";
+        const String RebuildResultMessage = "========== Rebuild All: 2 succeeded, 5 failed, 1 skipped ==========\r\n";
 
         [TestMethod]
         public void Build() {
             var span = CreateSpan(BuildResultMessage);
             BuildResult buildResult;
             var parsed = BuildResult.TryParse(span, out buildResult);
-            Assert.IsTrue(parsed);
-            Assert.IsNotNull(buildResult);
-            Assert.AreEqual(3,buildResult.Succeeded);
-            Assert.AreEqual(1, buildResult.Failed);
-            Assert.AreEqual(16, buildResult.UpToDate);
-            Assert.AreEqual(5, buildResult.Skipped);
+            Assert.IsTrue(parsed, "Not parsed");
+            Assert.IsNotNull(buildResult, "Not parsed");
+            Assert.AreEqual(3, buildResult.Succeeded, "Succeeded projects");
+            Assert.AreEqual(1, buildResult.Failed, "Failed projects");
+            Assert.AreEqual(16, buildResult.UpToDate, "Up-to-date projects");
+            Assert.AreEqual(5, buildResult.Skipped, "Skipped projects");
+        }
+
+        [TestMethod]
+        public void Rebuild() {
+            var span = CreateSpan(RebuildResultMessage);
+            BuildResult buildResult;
+            var parsed = BuildResult.TryParse(span, out buildResult);
+            Assert.IsTrue(parsed, "Not parsed");
+            Assert.IsNotNull(buildResult, "Not parsed");
+            Assert.AreEqual(2, buildResult.Succeeded, "Succeeded projects");
+            Assert.AreEqual(5, buildResult.Failed, "Failed projects");
+            Assert.AreEqual(0, buildResult.UpToDate, "Up-to-date projects");
+            Assert.AreEqual(1, buildResult.Skipped, "Skipped projects");
         }
 
         private SnapshotSpan CreateSpan(String text) {
--- VSOutputEnhancer/Parsers/BuildResult.cs
+++ VSOutputEnhancer/Parsers/BuildResult.cs
@@ -10,14 +10,14 @@ namespace Balakin.VSOutputEnhancer.Parsers {
             var text = span.GetText();
 
             result = null;
-            if (!text.StartsWith("========== Build: ", StringComparison.Ordinal)) {
+            if (!text.StartsWith("========== ", StringComparison.Ordinal)) {
                 return false;
             }
             if (!text.EndsWith(" ==========\r\n", StringComparison.Ordinal)) {
                 return false;
             }
 
-            var regex = "^========== Build: (?<Succeeded>\\d+) succeeded, (?<Failed>\\d+) failed, (?<UpToDate>\\d+) up-to-date, (?<Skipped>\\d+) skipped ==========\r\n$";
+            var regex = "^========== (?:Build|Rebuild All): (?<Succeeded>\\d+) succeeded, (?<Failed>\\d+) failed, (?:(?<UpToDate>\\d+) up-to-date, )?(?<Skipped>\\d+) skipped ==========\r\n$";
             var match = Regex.Match(text, regex);
             if (!match.Success) {
                 return false;
@@ -26,7 +26,9 @@ namespace Balakin.VSOutputEnhancer.Parsers {
             var localResult = new BuildResult();
             localResult.Succeeded = Convert.ToInt32(match.Groups["Succeeded"].Value);
             localResult.Failed = Convert.ToInt32(match.Groups["Failed"].Value);
-            localResult.UpToDate = Convert.ToInt32(match.Groups["UpToDate"].Value);
+            if (match.Groups["UpToDate"].Success) {
+                localResult.UpToDate = Convert.ToInt32(match.Groups["UpToDate"].Value);
+            }
             localResult.Skipped = Convert.ToInt32(match.Groups["Skipped"].Value);
             result = localResult;
             return true;
