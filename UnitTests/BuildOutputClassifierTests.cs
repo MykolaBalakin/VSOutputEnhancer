c327f9476005243359a965a7b1e6077903117682
Nikolay Balakin <n@balakin.me>
2015-11-22 17:56:35 +0200

    Add some more unit tests

--- UnitTests/BuildOutputClassifierTests.cs
+++ UnitTests/BuildOutputClassifierTests.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Balakin.VSOutputEnhancer.UnitTests.Stubs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 
 namespace Balakin.VSOutputEnhancer.UnitTests {
@@ -25,6 +26,21 @@ namespace Balakin.VSOutputEnhancer.UnitTests {
             var classificationSpan = result.Single();
             Assert.AreEqual(span, classificationSpan.Span);
             Assert.AreEqual(ClassificationType.BuildResultFailed, classificationSpan.ClassificationType.Classification);
+        }
+
+        [TestMethod]
+        public void BuildSucceeded() {
+            const String buildCompleteMessage = "========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========\r\n";
+
+            var span = Utils.CreateSpan(buildCompleteMessage);
+            var registry = CreateClassificationTypeRegistryService();
+            var classifier = new BuildOutputClassifier(registry);
+            var result = classifier.GetClassificationSpans(span);
+
+            Assert.AreEqual(1, result.Count);
+            var classificationSpan = result.Single();
+            Assert.AreEqual(span, classificationSpan.Span);
+            Assert.AreEqual(ClassificationType.BuildResultSucceeded, classificationSpan.ClassificationType.Classification);
         }
 
         [TestMethod]
@@ -55,6 +71,21 @@ namespace Balakin.VSOutputEnhancer.UnitTests {
             var classificationSpan = result.Single();
             Assert.AreEqual(span, classificationSpan.Span);
             Assert.AreEqual(ClassificationType.PublishResultSucceeded, classificationSpan.ClassificationType.Classification);
+        }
+
+        [TestMethod]
+        public void CompilationWarning() {
+            const String publishCompleteMessage = "1>C:\\Sources\\GitHub\\VSOutputEnhancer\\VSOutputEnhancer\\ClassificationType.cs(29,53,29,83): warning CS0169: The field 'ClassificationType.buildResultSucceededDefinition' is never used\r\n";
+
+            var span = Utils.CreateSpan(publishCompleteMessage);
+            var registry = CreateClassificationTypeRegistryService();
+            var classifier = new BuildOutputClassifier(registry);
+            var result = classifier.GetClassificationSpans(span);
+
+            Assert.AreEqual(1, result.Count);
+            var classificationSpan = result.Single();
+            Assert.AreEqual(new SnapshotSpan(span.Snapshot, 90, 91), classificationSpan.Span);
+            Assert.AreEqual(ClassificationType.BuildMessageWarning, classificationSpan.ClassificationType.Classification);
         }
 
         private IClassificationTypeRegistryService CreateClassificationTypeRegistryService() {
