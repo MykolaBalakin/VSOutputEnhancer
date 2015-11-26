f080e684a2cd89a83c4b736ce1464c5f7c4e3bf4
Nikolay Balakin <n@balakin.me>
2015-11-26 15:41:10 +0200

    Fix absent exports for some classification types

--- VSOutputEnhancer/Exports/ClassificationTypeExports.cs
+++ VSOutputEnhancer/Exports/ClassificationTypeExports.cs
@@ -11,18 +11,42 @@ namespace Balakin.VSOutputEnhancer.Exports {
     internal static class ClassificationTypeExports {
         [Export(typeof(ClassificationTypeDefinition))]
         [Name(ClassificationType.BuildResultSucceeded)]
-        private static ClassificationTypeDefinition buildResultSucceededDefinition;
+        public static ClassificationTypeDefinition BuildResultSucceeded;
 
         [Export(typeof(ClassificationTypeDefinition))]
         [Name(ClassificationType.BuildResultFailed)]
-        private static ClassificationTypeDefinition buildResultFailedDefinition;
+        public static ClassificationTypeDefinition BuildResultFailed;
 
         [Export(typeof(ClassificationTypeDefinition))]
         [Name(ClassificationType.BuildMessageError)]
-        private static ClassificationTypeDefinition buildMessageErrorDefinition;
+        public static ClassificationTypeDefinition BuildMessageError;
 
         [Export(typeof(ClassificationTypeDefinition))]
         [Name(ClassificationType.BuildMessageWarning)]
-        private static ClassificationTypeDefinition buildMessageWarningDefinition;
+        public static ClassificationTypeDefinition BuildMessageWarning;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name(ClassificationType.PublishResultFailed)]
+        public static ClassificationTypeDefinition PublishResultFailed;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name(ClassificationType.PublishResultSucceeded)]
+        public static ClassificationTypeDefinition PublishResultSucceeded;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name(ClassificationType.DebugTraceWarning)]
+        public static ClassificationTypeDefinition DebugTraceWarning;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name(ClassificationType.DebugTraceError)]
+        public static ClassificationTypeDefinition DebugTraceError;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name(ClassificationType.DebugTraceInformation)]
+        public static ClassificationTypeDefinition DebugTraceInformation;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name(ClassificationType.DebugException)]
+        public static ClassificationTypeDefinition DebugException;
     }
 }
