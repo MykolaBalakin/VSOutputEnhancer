47eb915bb0d848caf6f05e7c7e980d70e4930832
Nikolay Balakin <n@balakin.me>
2015-11-26 15:28:59 +0200

    Unit test to make sure that all classification definitions exported

--- UnitTests/ClassifierProviderTests.cs
+++ UnitTests/ClassifierProviderTests.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Balakin.VSOutputEnhancer.Exports;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Balakin.VSOutputEnhancer.UnitTests {
---
+++ UnitTests/ExportsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Balakin.VSOutputEnhancer.UnitTests {
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ExportsTests {
+        [TestMethod]
+        public void AllDefinitionsExported() {
+            var exportAttribute = typeof(ExportAttribute);
+            var classificationTypeDefinition=typeof(ClassificationTypeDefinition);
+            var assembly = typeof(ClassificationType).Assembly;
+            var types = assembly.GetTypes();
+            var exportedDefinitions = types
+                .SelectMany(t => t.GetFields(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public))
+                .Where(f => f.CustomAttributes.Any(a => a.AttributeType == exportAttribute))
+                .Where(f => f.GetCustomAttributes<ExportAttribute>().Any(a => a.ContractType == classificationTypeDefinition))
+                .SelectMany(f => f.GetCustomAttributes<NameAttribute>().Select(a => a.Name))
+                .Distinct()
+                .ToList();
+
+            var allDefinitions = ClassificationType.All;
+            var notExportedDefinitions = allDefinitions.Except(exportedDefinitions).ToList();
+            if (notExportedDefinitions.Any()) {
+                Assert.Fail("Not exported classification definitions: " + String.Join(", ", notExportedDefinitions));
+            }
+        }
+    }
+}
