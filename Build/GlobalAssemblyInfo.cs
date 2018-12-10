0ce9db2557eaff270e5885deb0ae53f6fee596eb
Rob Prouse <rob.prouse@bentley.com>
2018-12-10 13:37:06 -0500

    Update to support Visual Studio 2019

--- Build/GlobalAssemblyInfo.cs
+++ Build/GlobalAssemblyInfo.cs
@@ -12,5 +12,5 @@
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("1.6.*")]
-[assembly: AssemblyFileVersion("1.6")]
+[assembly: AssemblyVersion("1.7.*")]
+[assembly: AssemblyFileVersion("1.7")]
