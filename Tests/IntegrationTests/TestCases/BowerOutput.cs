4d2cf8000baa29ae744e567d73f049d6acb5e3da
Nikolay Balakin <n@balakin.me>
2019-07-11 12:46:25 +0300

    Add bower output test

---
+++ Tests/IntegrationTests/TestCases/BowerOutput.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Balakin.VSOutputEnhancer.Tests.IntegrationTests.TestCases
+{
+    [ExcludeFromCodeCoverage]
+    public class BowerOutput : ITestCase
+    {
+        public String ContentType { get; } = VSOutputEnhancer.ContentType.Output;
+
+        public IReadOnlyList<String> SourceText { get; } = new[]
+        {
+            "bower bootstrap1#3.3.5       ENOTFOUND Package bootstrap1 not found\r\n"
+        };
+
+        public IReadOnlyList<ClassifiedText> ExpectedResult { get; } = new[]
+        {
+            new ClassifiedText(ClassificationType.BowerMessageError, "Package bootstrap1 not found")
+        };
+    }
+}
