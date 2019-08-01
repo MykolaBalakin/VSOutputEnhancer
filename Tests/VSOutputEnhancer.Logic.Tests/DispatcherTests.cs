fec91135e1f6f7d951ac6f4afe620d1298d9ede4
Nikolay Balakin <n@balakin.me>
2019-08-01 16:09:51 +0300

    Implement Dispatcher

---
+++ Tests/VSOutputEnhancer.Logic.Tests/DispatcherTests.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+
+namespace Balakin.VSOutputEnhancer.Logic.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class DispatcherTests
+    {
+        private class TestEvent : IEvent
+        {
+        }
+
+        private class AnotherTestEvent : IEvent
+        {
+        }
+
+        private class TestEventHandler<TEvent> : IEventHandler<TEvent> where TEvent : IEvent
+        {
+            public IEnumerable<String> ContentTypes { get; }
+            public Int32 InvocationCount { get; private set; }
+
+            public void Handle(TEvent @event)
+            {
+                InvocationCount++;
+            }
+        }
+
+        [Fact]
+        public void AddHandlerDoesNotThrowException()
+        {
+            var dispatcher = new Dispatcher();
+            var handler = new TestEventHandler<TestEvent>();
+            dispatcher.AddHandler(handler);
+        }
+
+        [Fact]
+        public void DispatchInvokesHandler()
+        {
+            var dispatcher = new Dispatcher();
+            var handler = new TestEventHandler<TestEvent>();
+            dispatcher.AddHandler(handler);
+
+            var @event = new TestEvent();
+            dispatcher.Dispatch(@event);
+
+            handler.InvocationCount.Should().Be(1);
+        }
+
+        [Fact]
+        public void DispatchInvokesOnlyAppropriateHandler()
+        {
+            var dispatcher = new Dispatcher();
+            var handler = new TestEventHandler<TestEvent>();
+            dispatcher.AddHandler(handler);
+
+            var anotherHandler = new TestEventHandler<AnotherTestEvent>();
+            dispatcher.AddHandler(anotherHandler);
+
+            var @event = new TestEvent();
+            dispatcher.Dispatch(@event);
+
+            handler.InvocationCount.Should().Be(1);
+            anotherHandler.InvocationCount.Should().Be(0);
+        }
+    }
+}
--- VSOutputEnhancer.Logic/Dispatcher.cs
+++ VSOutputEnhancer.Logic/Dispatcher.cs
@@ -1,16 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Balakin.VSOutputEnhancer.Logic
 {
     public class Dispatcher : IDispatcher
     {
+        private readonly IDictionary<Type, Action<Object>> eventHandlers = new Dictionary<Type, Action<Object>>();
+
         public void AddHandler(IEventHandler handler)
         {
-            throw new NotImplementedException();
+            foreach (var eventType in EnumerateEventTypes(handler))
+            {
+                AddEventHandler(handler, eventType);
+            }
         }
 
         public void Dispatch(IEvent @event)
         {
+            var type = @event.GetType();
+            while (type != null)
+            {
+                InvokeHandlers(@event, type);
+                type = type.BaseType;
+            }
+        }
+
+        private IEnumerable<Type> EnumerateEventTypes(IEventHandler handler)
+        {
+            var eventHandlerInterface = typeof(IEventHandler<>);
+            var allInterfaces = handler.GetType().GetInterfaces();
+            return allInterfaces
+                .Where(i => i.IsGenericType)
+                .Where(i => i.GetGenericTypeDefinition() == eventHandlerInterface)
+                .Select(i => i.GetGenericArguments().Single());
+        }
+
+        private void AddEventHandler(IEventHandler handler, Type eventType)
+        {
+            var genericMethodDefinition = typeof(Dispatcher)
+                .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                .Where(m => m.Name == nameof(AddEventHandler))
+                .Single(m => m.IsGenericMethodDefinition);
+
+            var genericMethod = genericMethodDefinition.MakeGenericMethod(eventType);
+            genericMethod.Invoke(this, new Object[] { handler });
+        }
+
+        private void AddEventHandler<TEvent>(IEventHandler<TEvent> handler) where TEvent : IEvent
+        {
+            Action<Object> handlerDelegateToAdd = (@event) => handler.Handle((TEvent)@event);
+            if (eventHandlers.TryGetValue(typeof(TEvent), out var handlerDelegate))
+            {
+                handlerDelegateToAdd = handlerDelegate + handlerDelegateToAdd;
+            }
+
+            eventHandlers[typeof(TEvent)] = handlerDelegateToAdd;
+        }
+
+        private void InvokeHandlers(Object @event, Type eventType)
+        {
+            {
+                if (eventHandlers.TryGetValue(eventType, out var handler))
+                {
+                    handler(@event);
+                }
+            }
         }
     }
 }
