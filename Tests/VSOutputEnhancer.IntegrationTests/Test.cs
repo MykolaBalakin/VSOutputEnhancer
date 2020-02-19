f4b58ac753e392043a427ec3ddc894352a6817b4
Nikolay Balakin <n@balakin.me>
2020-02-19 16:40:10 +0200

    Change integration test event handling logic to follow Visual Studio behaviour

--- Tests/VSOutputEnhancer.IntegrationTests/Test.cs
+++ Tests/VSOutputEnhancer.IntegrationTests/Test.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using Balakin.VSOutputEnhancer.Logic;
+using Balakin.VSOutputEnhancer.Tests.Base;
 using Balakin.VSOutputEnhancer.Tests.Base.Stubs;
 using FluentAssertions;
 using Microsoft.VisualStudio.Text;
@@ -27,17 +27,19 @@ namespace Balakin.VSOutputEnhancer.IntegrationTests
             var classifier = classifierProvider.GetClassifier(new TextBufferStub(testCase.ContentType));
 
             var actualResult = new List<ClassifiedText>();
+
+            var spansToReclassify = new List<SnapshotSpan>();
+            classifier.ClassificationChanged += (sender, args) => spansToReclassify.Add(args.ChangeSpan);
+
             foreach (var line in testCase.SourceText)
             {
-                var snapshot = new TextSnapshotStub(line);
-                var span = new SnapshotSpan(snapshot, new Span(0, snapshot.Length));
-                var classificationSpans = classifier.GetClassificationSpans(span);
-                foreach (var classificationSpan in classificationSpans)
+                var span = line.ToSnapshotSpan();
+                DoClassification(classifier, span, actualResult);
+                foreach (var spanToReclassify in spansToReclassify)
                 {
-                    var classifiedText = classificationSpan.Span.GetText();
-                    var classificationType = classificationSpan.ClassificationType.Classification;
-                    actualResult.Add(new ClassifiedText(classificationType, classifiedText));
+                    DoClassification(classifier, spanToReclassify, actualResult);
                 }
+                spansToReclassify.Clear();
             }
 
             actualResult.Should().BeEquivalentTo(testCase.ExpectedResult);
@@ -61,6 +63,17 @@ namespace Balakin.VSOutputEnhancer.IntegrationTests
             var container = new CompositionContainer(catalog);
             var classifierProvider = container.GetExport<IClassifierProvider>();
             return classifierProvider.Value;
+        }
+
+        private void DoClassification(IClassifier classifier, SnapshotSpan span, IList<ClassifiedText> result)
+        {
+            var classificationSpans = classifier.GetClassificationSpans(span);
+            foreach (var classificationSpan in classificationSpans)
+            {
+                var classifiedText = classificationSpan.Span.GetText();
+                var classificationType = classificationSpan.ClassificationType.Classification;
+                result.Add(new ClassifiedText(classificationType, classifiedText));
+            }
         }
     }
 }
